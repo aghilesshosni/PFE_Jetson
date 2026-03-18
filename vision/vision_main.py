3d42c1a7a6ca6546ae3bc8ecd478d58267b24632
Amin Hosni <aghilesshosni@gmail.com>
2026-03-18 16:34:32 +0100

    Créeation des fonction init() et start() dans main_vision.py dans vision

--- vision/vision_main.py
+++ vision/vision_main.py
@@ -5,4 +5,15 @@ from vision import (GestionnaireCamera, ConfigurateurCamera, AnalyseurPosition, 
 
 
 
-class VisionMain: 
+class VisionMain:
+	def __init__(self):
+		self.config= ConfigurateurCamera()
+		self.camera= GestionnaireCamera(camera_id=0)
+                #Preallocation de buffer
+		self.frame_buffer=np.zeros((self.ConfigurateurCamera.height, self.ConfigurateurCamera.width, 3),dtype=np.uint8)
+		self.est_tournant=False
+		self.dernier_resultat={'Autorisée':'False', 'Compensation':'0'}
+	def start():
+		if not self.camera.open():
+			raise RuntimeError("Echec de Camera")
+		self.est_tournant=True 
