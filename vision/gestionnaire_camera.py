03eee08c83ea946edd41b09d1b475ea2a96c8cac
Amin Hosni <aghilesshosni@gmail.com>
2026-04-10 10:47:07 +0100

    Changement de gestionnaire_camera.py

--- vision/gestionnaire_camera.py
+++ vision/gestionnaire_camera.py
@@ -4,11 +4,33 @@ class GestionnaireCamera:
     def __init__(self, camera_id):
         self.camera_id = camera_id
         self.cap = None
+        self.gstreamer_pipeline = (
+            "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! "
+            "nvvidconv flip-method=0 ! "
+            "video/x-raw, width=(int)1920, height=(int)1080, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! "
+            "appsink"
+        )
 
     def open(self):
-        self.cap = cv2.VideoCapture(self.camera_id)
+        if isinstance(self.camera_id, int):
+            self.cap = cv2.VideoCapture(self.camera_id)
+            
+            if not self.cap.isOpened():
+                print("Échec ouverture , on essaye GStreamer pour CSI...")
+                self.cap = cv2.VideoCapture(self.gstreamer_pipeline, cv2.CAP_GSTREAMER)
+        else:
+            self.cap = cv2.VideoCapture(self.camera_id, cv2.CAP_GSTREAMER)
+
         if not self.cap.isOpened():
             return False
+        
+        ret, _ = self.cap.read()
+        if not ret:
+            return False
+            
         return True
 
     def read(self):
