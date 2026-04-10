ad793c614341a66daad25262bc13de0862a3989b
Amin Hosni <aghilesshosni@gmail.com>
2026-04-10 13:58:16 +0200

    g

--- vision/gestionnaire_camera.py
+++ vision/gestionnaire_camera.py
@@ -4,6 +4,7 @@ class GestionnaireCamera:
     def __init__(self, camera_id):
         self.camera_id = camera_id
         self.cap = None
+        
         self.gstreamer_pipeline = (
             "nvarguscamerasrc ! "
             "video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! "
@@ -15,14 +16,18 @@ class GestionnaireCamera:
         )
 
     def open(self):
-        self.cap = cv2.VideoCapture(self.gstreamer_pipeline,cv2.CAP_GSTREAMER)
-            
-
+        print(f"Tentative d'ouverture de la caméra...")
+        
+        print("Utilisation du pipeline GStreamer NVARGUS...")
+        self.cap = cv2.VideoCapture(self.gstreamer_pipeline, cv2.CAP_GSTREAMER)
+        
         if not self.cap.isOpened():
+            print("   Vérifie que le pipeline est correct pour ta caméra.")
             return False
         
-        ret, _ = self.cap.read()
-        if not ret:
+        ret, frame = self.cap.read()
+        if not ret or frame is None:
+            self.cap.release()
             return False
             
         return True
@@ -32,9 +37,12 @@ class GestionnaireCamera:
             return None
         ret, frame = self.cap.read()
         if not ret:
-            return None
+            ret, frame = self.cap.read()
+            if not ret:
+                return None
         return frame
 
     def release(self):
         if self.cap is not None:
             self.cap.release()
+            self.cap = None
